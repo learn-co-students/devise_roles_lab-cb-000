c83e8c6547f68de01a0b0f2276e7ce929112ccb1
Michael Anderson <michanders@msn.com>
2018-02-06 01:21:18 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'users#index'
+  post 'destroy' => 'users#destroy'
+  post 'create' => 'posts#create'
+  post 'update' => 'posts#update'
 
-  resources :users
+  resources :users, :posts
 end
