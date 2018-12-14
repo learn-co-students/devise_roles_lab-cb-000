d701d517791023c74f51584a323251c504962784
Jeremy Parker <jrobinparker@gmail.com>
2018-12-14 23:53:04 +0000

    Need to fix users and posts controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    if !current_user.admin? && current_user != @user
+      flash[:alert] = "Access denied."
+      redirect_to root_path
+    end 
+  end
+
+  def destroy
+    session.destroy
+  end
+
 end
