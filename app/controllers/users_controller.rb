02828c614ee4cf29e450d5a7547f7eaedebe34a7
Will Hodgson <will.hodgson@current-rms.com>
2019-11-21 09:09:52 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+  def destroy
+    @user = User.find(params[:id])
+    return head(:forbidden) unless current_user.admin?
+    @user.destroy
+  end
+
+  def show
+    if current_user.id != params[:id]
+      redirect_to users_path, notice: 'Access denied'
+    else
+      @user = User.find(params[:id])
+    end
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
 end
